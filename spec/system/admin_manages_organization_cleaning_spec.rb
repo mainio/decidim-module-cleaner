1d1f188352bcb5134a62e709d310799790a5a6c0
Joonas <joonas.aapro@mainiotech.fi>
2026-08-04 11:32:31 +0300

    Fix specs

--- spec/system/admin_manages_organization_cleaning_spec.rb
+++ spec/system/admin_manages_organization_cleaning_spec.rb
@@ -2,7 +2,7 @@
 
 require "spec_helper"
 
-describe "Admin manages organization cleaning" do
+describe "Cleaning" do
   let(:organization) { create(:organization) }
   let(:user) { create(:user, :admin, :confirmed, organization:) }
 
